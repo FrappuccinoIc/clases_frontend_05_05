0bb7200f698613be081db955fd40c176a38329d0
frappuccinoic <arseeicore@gmail.com>
2025-05-05 16:25:24 -0400

    Añadiendo a tabla con manipulación DOM

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const students = [];
 
-const ulStudents = document.getElementById("lista-notas");
+const tableBody = document.querySelector("#studentsTable tbody")
 const form = document.getElementById("studentForm");
 
 form.addEventListener('submit', (e) => {
@@ -12,15 +12,22 @@ form.addEventListener('submit', (e) => {
 
     if( name.length < 3 || name.length > 30 ||
         lastName.length < 3 || lastName.length > 30 ||
-        grade < 1.0 || grade > 7.0
+        grade < 1.0 || grade > 7.0 || isNaN(grade)
     ) return alert("Error, Datos Incorrectos.");
 
     const student = {name, lastName, grade};
     students.push(student);
 
-    // const registro = document.createElement("li");
-    // const contenidoRegistro = document.createTextNode(`Nombre: ${name}, Apellido: ${lastName}, Nota: ${grade}`);
-    // registro.appendChild(contenidoRegistro);
-
-    console.log(`Nombre: ${name}, Apellido: ${lastName}, Nota: ${grade}`);
+    addStudentToTable(student);
 })
+
+function addStudentToTable(student) {
+    const row = document.createElement("tr");
+    row.innerHTML = 
+    `
+        <td>${student.name}</td>
+        <td>${student.lastName}</td>
+        <td>${student.grade }</td>
+    `;
+    tableBody.appendChild(row);
+}
